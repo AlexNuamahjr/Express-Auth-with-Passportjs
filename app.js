ea30eca48bfe0432811f1825b22ff67915dc18a8
AlexNuamah <alexandernuamahjr@gmail.com>
2023-09-27 12:14:26 +0000

    Update: Database Sequelize(ORM) connection

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const dbConnect = require('./dbConnect');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.listen(PORT, ()=>{
+    dbConnect.authenticate();
     console.log(`Server is running on http://localhost:${PORT}`);
 })
 
